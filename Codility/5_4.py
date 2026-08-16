f1310dfb5890092881c01d43d3c111ef06e7d117
tos <dltosy15@gmail.com>
2026-08-16 21:45:26 +0900

    Codility Study

--- Codility/5_4.py
+++ Codility/5_4.py
@@ -1,21 +1,22 @@
 ##
 
 def solution(A):
-    # Implement your solution here
-    n = len(A)
-    min_idx = 0
-    min_avg = (A[0] + A[1]) / 2.0 # arbitrary avg
+    N = len(A)
+    min_avg = (A[0] + A[1]) / 2.0 # arbitrary
+    min_pos = 0
 
-    for i in range(n-1):
-        avg2 = (A[i] + A[i+1]) / 2.0
+    for P in range(N - 1):
+        avg2 = (A[P] + A[P + 1]) / 2.0
         if avg2 < min_avg:
             min_avg = avg2
-            min_idx = i
-        if i < n-2:
-            avg3 = (A[i] + A[i+1] + A[i+2]) / 3.0
+            min_pos = P
+
+        if P < N - 2:
+            avg3 = (A[P] + A[P + 1] + A[P + 2]) / 3.0
             if avg3 < min_avg:
                 min_avg = avg3
-                min_idx = i
-    return min_idx
+                min_pos = P
+
+    return min_pos
 
 print(solution([4, 2, 2, 5, 1, 5, 8]))
