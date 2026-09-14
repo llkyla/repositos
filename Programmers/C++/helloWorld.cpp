3cfaaf69f2fc4d5183a04816aecfb8b136621687
tos <dltosy15@gmail.com>
2026-09-14 12:26:17 +0900

    C++ Study

---
+++ Programmers/C++/helloWorld.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+int main(){
+
+    return 0;
+}
+// 1. Basic Basic
+/*
+int main(){
+
+    // comments
+
+
+    std::cout << "I like pizza!" << std::endl;
+    std::cout << "It's really good!" << '\n';
+
+    return 0;
+}
+*/
+
+// 2. var type
+/*
+int main() {
+
+    int age = 21; //declaration - assignment
+    int year = 2023;
+    int days = 7;
+
+    double price = 10.99;
+    double temperature = 25.1;
+
+    char grade = 'A';
+    char initial = 'B';
+    char currency = '$';
+
+    bool studnet = true;
+    bool power = true;
+
+    std::string name = "Bro";
+    std::string day = "Friday";
+    std::string food = "pizza";
+
+
+    std::cout << "Hello " << name << std::endl;
+    std::cout << "You are " << age << " years old.";
+
+    return 0;
+}
+*/
+
+//3. Const
+/*
+int main() {
+    const double PI = 3.141592; //prefix the value; no one can change
+    const int LIGHT_SPEED = 299892458;
+    double radius = 10;
+    double circumference = 2 * PI * radius;
+
+    std::cout << circumference << "cm";
+
+    return 0;
+}
+*/
+
+// 4. Namespace
+/*
+namespace first{
+    int x = 1;
+}
+
+namespace second{
+    int x = 2;
+}
+int main() {
+
+    using namespace first; // assume to use namespace first
+    using namespace std; // std:: not using 
+
+    int x = 0;
+
+    std::cout << second::x;
+
+    return 0;
+}
+*/
+
+// 5. typedef
+#include <vector>
+
+//typedef std::
+int main(){
+
+    return 0;
+}
