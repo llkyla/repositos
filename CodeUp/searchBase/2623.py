651ff8bb5c91f80599abfb7250c4a572969ce69d
tos <dltosy15@gmail.com>
2026-09-16 17:39:50 +0900

    searchBase Study

---
+++ CodeUp/searchBase/2623.py
@@ -0,0 +1,19 @@
+# 2623
+
+def gcd(a, b):
+    if b == 0:
+        return a
+    return gcd(b, a % b)
+
+'''
+def lcm (a,b):
+    return a * b // gcd(a, b)
+'''
+a, b = map(int, input().split(' '))
+print(gcd(a,b))
+
+'''
+import math
+print(math.gcd(a,b))
+print(math.lcm(a.b))
+'''
